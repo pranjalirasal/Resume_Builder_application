9f31ee8fe462da7f03f076779ba765e6e28e5e01
pranjalirasal <pranjalirasal782004@gmail.com>
2025-08-03 12:30:28 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load .env file variables
+dotenv.config(); // 
 
 export const connectDB = async () => {
   await mongoose
